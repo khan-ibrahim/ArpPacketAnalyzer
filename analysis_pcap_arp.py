ccdf6953af2ffdcdb2de3eb7dd9870b618e1ac2d
Ibrahim <5855449+khan-ibrahim@users.noreply.github.com>
2019-04-28 23:27:49 -0400

    formats output instead of raw bytes.

--- analysis_pcap_arp.py
+++ analysis_pcap_arp.py
@@ -1,4 +1,5 @@
 import dpkt
+import socket
 
 def getARP(pcapFileName):
 
@@ -13,22 +14,22 @@ def getARP(pcapFileName):
             arpPackets.append([ts, eth.arp.__bytes__()])
     return arpPackets
 
-def printArpPkt(bytes):
+def printArpPkt(pkt):
+    print('Hardware type:{}'.format(int.from_bytes(pkt[0:2], byteorder='big')))
+    print('Protocol type:{}'.format(int.from_bytes(pkt[2:4], byteorder='big')))
+    print('Hardware size:{}'.format(pkt[4]))
+    print('Protocol size:{}'.format(pkt[5]))
+    print('Opcode:{}'.format(int.from_bytes(pkt[6:8], byteorder='big')))
+    print('Sender MAC address:{}'.format((pkt[8:14]).hex()))
+    print('Sender IP address:{}'.format(socket.inet_ntoa(pkt[14:18])))
+    print('Target MAC address:{}'.format((pkt[18:24]).hex()))
+    print('Target IP address:{}'.format(socket.inet_ntoa(pkt[24:28])))
     return
 
 def printPackets(arpPackets):
-    i = 0
     for ts, pkt in arpPackets:
         print('Time Stamp:{}-----------------'.format(ts))
-        print('Hardware type:{}'.format(pkt[0:2]))
-        print('Protocol type:{}'.format(pkt[2:4]))
-        print('Hardware size:{}'.format(pkt[4]))
-        print('Protocol size:{}'.format(pkt[5]))
-        print('Opcode:{}'.format(pkt[6:8]))
-        print('Sender MAC address:{}'.format(pkt[8:14]))
-        print('Sender IP address:{}'.format(pkt[14:18]))
-        print('Target MAC address:{}'.format(pkt[18:24]))
-        print('Target IP address:{}'.format(pkt[24:28]))
+        printArpPkt(pkt)
         print()
 
 def main(pcapFileName):
