6e9b1f85b2ebdc1e1a8c24babfbb176c8bc26520
Ibrahim <5855449+khan-ibrahim@users.noreply.github.com>
2019-04-29 17:59:34 -0400

    format MAC addresses with colons.

--- analysis_pcap_arp.py
+++ analysis_pcap_arp.py
@@ -17,15 +17,18 @@ def getARP(pcapFileName):
             arpPackets.append([ts, eth.arp.__bytes__()])
     return arpPackets
 
+def formatMacBits(macBits):
+    return '{}:{}:{}:{}:{}:{}'.format(macBits[0:2],macBits[2:4],macBits[4:6],macBits[6:8],macBits[8:10],macBits[10:12])
+
 def printArpPkt(pkt):
     print('Hardware type:{}'.format(int.from_bytes(pkt[0:2], byteorder='big')))
     print('Protocol type:{}'.format(int.from_bytes(pkt[2:4], byteorder='big')))
     print('Hardware size:{}'.format(pkt[4]))
     print('Protocol size:{}'.format(pkt[5]))
     print('Opcode:{}'.format(int.from_bytes(pkt[6:8], byteorder='big')))
-    print('Sender MAC address:{}'.format((pkt[8:14]).hex()))
+    print('Sender MAC address:{}'.format(formatMacBits(pkt[8:14].hex())))
     print('Sender IP address:{}'.format(socket.inet_ntoa(pkt[14:18])))
-    print('Target MAC address:{}'.format((pkt[18:24]).hex()))
+    print('Target MAC address:{}'.format(formatMacBits(pkt[18:24].hex())))
     print('Target IP address:{}'.format(socket.inet_ntoa(pkt[24:28])))
     return
 
